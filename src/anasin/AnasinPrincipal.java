9da37a7dfcb419fcd33ebc320fdafc78e06f05c9
Wellington Umbelino <41035606+wellingtonumbelino@users.noreply.github.com>
2019-10-03 20:37:55 -0300

    Update AnasinPrincipal.java

--- src/anasin/AnasinPrincipal.java
+++ src/anasin/AnasinPrincipal.java
@@ -2,7 +2,7 @@ package anasin;
 
 public class AnasinPrincipal {
 	public static void main(String[] args) {
-		String exp = "x = 2.1 + 1";
+		String exp = "> x = 4.1 ^ 3; soma = total + (3 + y) !";
 		AnasinSimp anasin = new AnasinSimp(exp);
 		anasin.anasint();
 	}
